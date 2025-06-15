ba68f465e51ae07e51b2298db215e187544696ef
Ökkeş Amoca <okkes2001@hotmail.fr>
2025-06-15 15:47:48 +0200

    Front & back

--- HLuxuryDriving-Server/obj/Debug/net7.0/HLuxuryDriving-Server.AssemblyInfo.cs
+++ HLuxuryDriving-Server/obj/Debug/net7.0/HLuxuryDriving-Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HLuxuryDriving-Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9c40fe6cb3b2dd0d034ab164254291f62d9aa25")]
 [assembly: System.Reflection.AssemblyProductAttribute("HLuxuryDriving-Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HLuxuryDriving-Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
