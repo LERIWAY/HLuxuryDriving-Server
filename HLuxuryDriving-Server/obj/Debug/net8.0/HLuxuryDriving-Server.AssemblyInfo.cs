d03d08fd135c310efeee7a3cc8c6190c3dd5cf98
Ökkeş Amoca <okkes2001@hotmail.fr>
2025-08-11 14:48:16 +0200

    fixe css

--- HLuxuryDriving-Server/obj/Debug/net8.0/HLuxuryDriving-Server.AssemblyInfo.cs
+++ HLuxuryDriving-Server/obj/Debug/net8.0/HLuxuryDriving-Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HLuxuryDriving-Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3ba07008ec40cfb203e72c907261b5bd9ceb291")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+802f94d3e5c94ee847ec547942358fc99dd313a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("HLuxuryDriving-Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HLuxuryDriving-Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
